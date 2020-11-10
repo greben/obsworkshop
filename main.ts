cfc31eb8b526c14ed7cdcbd41c6761012a25389a
Gre Ben <greben@users.noreply.github.com>
2020-11-10 14:16:15 -0700

    Multi-Tasking & Chaining

--- main.ts
+++ main.ts
@@ -1,35 +1,24 @@
-import { Observable,  } from 'rxjs'; 
-import { share } from 'rxjs/operators';
+import { interval, Observable,  } from 'rxjs'; 
+import { filter, map, pairwise,take } from 'rxjs/operators';
 
 console.clear();
 
-// in FileA
-const o = new Observable(observer => {
-    console.log("About to do http");
-    // pretend we did http
-    setTimeout(() => {
-        console.log("Did http");
-        observer.next([5,6,7]);    
-    }, 1000);
-    
-}); // .pipe(share())
-// share() will make this only create the observable once
-
-// in FileB
-// this create a new observable that is shared, but does not modify the original observable o
-// can only mutated can't modify
-// since the new observable is shared it will only be created once
-const myObs = o.pipe(share());
-
-myObs.subscribe(console.log);
-myObs.subscribe(console.log);
-myObs.subscribe(console.log);
+// basically an interval observable
+// const o = new Observable(observer => {
+//     let count = 0;
+//     setTimeout(() => {
+//         observer.next(count++);    
+//     }, 1000);
+// }); 
 
 
-// in FileC 
-// subscribe to the FileA observable that is not shared, will create the observable with each subscription 
-o.subscribe(console.log);
-o.subscribe(console.log);
+// How you do chaining with observables
+const o = interval(1000).pipe(
+    filter(num => num % 2 == 0),
+    map(num => num * 3),
+    pairwise()
+)
+
 o.subscribe(console.log);
 
 
