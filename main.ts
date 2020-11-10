b22adb0c7aeb448073a0efcefe0cdecbcbf3e0cb
Gre Ben <greben@users.noreply.github.com>
2020-11-10 12:26:11 -0700

    Eager vs Lazy

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
+import { Observable } from 'rxjs';
+
+// Eager vs Lazy
 console.clear();
 
-// See the README.MD for what code we will be writing in this section.
+const promise = new Promise((resolve, reject) => {
+    console.log('Creating Promise');
+    resolve([1,2,3])
+});
+
+const observable = new Observable(observer => {
+    console.log('Creating Observable');
+    observer.next([4,5,6]);
+});
+
+// if you don't subscribe the observable doesn't get created
+// the promise is eager so it fires when it is created
+//promise.then(result => console.log(result));
+//observable.subscribe(result => console.log(result));
+
+/*
+Creating Promise
+Creating Observable
+[ 4, 5, 6 ]
+Creating Observable
+[ 4, 5, 6 ]        
+Creating Observable
+[ 4, 5, 6 ]    
+*/
+
+//promise.then(result => console.log(result));
+
+// promise.then(
+//     result => {
+//         console.log(result);
+//     },
+//         err => console.log(err)
+// );
+
+//observable.subscribe(result => console.log(result));
+
+// observable.subscribe(
+//     result => {
+//         console.log(result);
+//     },
+//         err => console.log(err)
+// );
+
+setTimeout(() => {
+    observable.subscribe(result => console.log(result));
+}, 2000);
